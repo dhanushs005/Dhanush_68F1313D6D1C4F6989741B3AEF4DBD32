c6d0745478c07ac001fe35549688b5c676a2cdc9
dhanushs005 <144521278+dhanushs005@users.noreply.github.com>
2023-09-21 19:09:48 +0530

    Update Challenge 1.py

--- Challenge unit 2/Challenge 1.py
+++ Challenge unit 2/Challenge 1.py
@@ -7,26 +7,26 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+            print(f"Deposited Rs.{amount}. New balance: Rs.{self.__account_balance}")
         else:
             print("Invalid deposit amount. Amount must be greater than zero.")
 
     def withdraw(self, amount):
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+            print(f"Withdrew Rs.{amount}. New balance: Rs.{self.__account_balance}")
         else:
             print("Invalid withdrawal amount or insufficient funds.")
 
     def display_balance(self):
-        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
+        print(f"Account Balance for {self.__account_holder_name}: Rs.{self.__account_balance}")
 
 
 
-account1 = BankAccount("12345", "John Doe", 1000)
+account1 = BankAccount("12345", "Dhanush", 1000)
 
 
 account1.display_balance()
-account1.deposit(500)
-account1.withdraw(200)
+account1.deposit(700)
+account1.withdraw(400)
 account1.display_balance()
