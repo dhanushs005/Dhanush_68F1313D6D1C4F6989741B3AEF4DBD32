174ea63518da10b7c65c005e860664cb8b5bc085
dhanushs005 <144521278+dhanushs005@users.noreply.github.com>
2023-09-28 18:53:45 +0530

    Delete Challenge unit 4 directory

--- Challenge unit 4/Challenge 2.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-my_date_string = "Jun 10 2005 11:31AM"
-
-datetime_object = datetime.strptime(my_date_string, '%b %d %Y %I:%M%p')
-
-print(type(datetime_object))
-print(datetime_object)
